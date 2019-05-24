af5fd53fc23ecc134b8651bb74eaf4d1876c2be1
Brad Cantrell <brad777mx@hotmail.com>
2019-05-24 09:22:27 -0700

    update

--- api/server.spec.js
+++ api/server.spec.js
@@ -8,22 +8,41 @@ describe("server.js", () => {
     })
 
     describe("endpoints", () => {
-        describe("GET /", () => {
+        describe("GET /games", () => {
             // test for res.status
             it("should return 200 OK using async/await THE SQUAD", async () => {
-                const res = await request(server).get("/")
+                const res = await request(server).get("/games")
                 expect(res.status).toBe(200)
             })
 
             // test for res.type JSON
             it("should return JSON", async () => {
-                const res = await request(server).get("/")
+                const res = await request(server).get("/games")
                 expect(res.type).toBe("application/json")
             })
 
             // test for res.body
             it('should return { api: "up" }', async () => {
-                const res = await request(server).get("/")
+                const res = await request(server).get("/games")
+                expect(res.body).toEqual({ api: "up" })
+            })
+        })
+        describe("POST /games", () => {
+            // test for res.status
+            it("should return 200 OK using async/await THE SQUAD", async () => {
+                const res = await request(server).post("/games")
+                expect(res.status).toBe(200)
+            })
+
+            // test for res.type JSON
+            it("should return JSON", async () => {
+                const res = await request(server).post("/games")
+                expect(res.type).toBe("application/json")
+            })
+
+            // test for res.body
+            it('should return { api: "up" }', async () => {
+                const res = await request(server).post("/games")
                 expect(res.body).toEqual({ api: "up" })
             })
         })
